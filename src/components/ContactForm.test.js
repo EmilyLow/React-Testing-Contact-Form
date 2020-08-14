53dd41a0d1c1553f7876c5fcd4824e05418bedba
EmilyLow <emlow@me.com>
2020-08-13 21:00:22 -0500

    Test basic data

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -2,18 +2,22 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 //Should this be necessary?
 import userEvent from '@testing-library/user-event';
-
 import ContactForm from "./ContactForm";
+
+//Should we be able to test actually submitting with various data?
 
 test("renders App without crashing", () => {
   render(<ContactForm />);
 });
 
-test("test entering name", () => {
+test("test entering correct data", () => {
 
   const {getByLabelText} = render(<ContactForm/>)
   let name = "Em";
   const firstNameField = getByLabelText(/First Name/i);
+  const lastNameField = getByLabelText(/Last Name/i);
+  const emailField = getByLabelText(/Email/i);
+  const messageField = getByLabelText(/Message/i);
 //  getByLabelText(/Last Name/i);
   
   // fireEvent.change(firstNameField, {
@@ -22,5 +26,15 @@ test("test entering name", () => {
 
   userEvent.type(firstNameField, "Em"
   );
+  userEvent.type(lastNameField, "Smith"
+  );
+  userEvent.type(emailField, "fakeEmail@gmail.com"
+  );
+//   messageField.type(messageField, "This is a message"
+//   );
+
+  //fireEvent.change(firstNameInput, )
+
+//   fireEvent.click(submitButton);
 
 });
