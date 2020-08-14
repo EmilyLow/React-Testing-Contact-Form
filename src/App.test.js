5ed1bddb58be7395ddd139fcd1dad871214f9fb5
EmilyLow <emlow@me.com>
2020-08-13 20:16:36 -0500

    Attempting tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
+});
+
+test("test entering name", () => {
+
+  const {getByLabelText} = render(<App/>)
+  let name = "Em";
+  const firstNameField = getByLabelText(/first name*/i);
+  
+  fireEvent.change(firstNameField, {
+    target: {name: "firstName", value: "Em"}
+  });
+
 });
